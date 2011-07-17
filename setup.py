022f48053138bb8756a634b907c0965e6113802e
Marije Baalman <nescivi@hypermetheus.(none)>
2011-07-17 13:37:42 +0200

    made a setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, findpackages
+
+setup(name='Pydon',
+      version='0.6',
+      description='Python packages for the Sense/Stage DataNetwork',
+      author='Marije Baalman',
+      author_email='sensestage@nescivi.nl',
+      url='http://www.sensestage.eu',
+      packages=find_packages(),
+      install_requires=[
+         'PyOSC>=0.3',
+         'pyserial>=2.5'
+    ]
+)
