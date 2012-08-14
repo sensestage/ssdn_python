7c5f1d1ef8e010299bedc4fed09b596c46b354f1
Marije Baalman <nescivi@hypermetheus.(none)>
2012-08-14 12:23:14 +0200

    exit in separate function

--- pydon/pydon/minihivejunxion.py
+++ pydon/pydon/minihivejunxion.py
@@ -304,17 +304,20 @@ class SWMiniHiveJunxion( object ):
     self.verbose = verbose
 
     self.hive.set_newBeeAction( self.hookBeeToOSC )
-        
+
+  def exit( self ):
+    self.osc.osc.close()
+    print( "Waiting for Server-thread to finish" )
+    self.osc.thread.join() ##!!!
+    print( "Done; goodbye" )
+    self.hive.exit()
+    sys.exit()
+
   def start( self ):
     try :
       self.hive.run()
     except (SystemExit, RuntimeError,KeyboardInterrupt, IOError ) :
-      self.osc.osc.close()
-      print( "Waiting for Server-thread to finish" )
-      self.osc.thread.join() ##!!!
-      print( "Done; goodbye" )
-      self.hive.exit()
-      sys.exit()
+      self.exit()
 
   
   #def setMapAction( self, nodeid, mid ):
