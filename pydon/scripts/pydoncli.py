914c2212e318fadd6197c66bacb79c9cfe6b2520
Marije Baalman <marije@nescivi.nl>
2013-10-01 07:35:15 +0200

    add logging to cli version

--- pydon/scripts/pydoncli.py
+++ pydon/scripts/pydoncli.py
@@ -32,11 +32,54 @@ from pydon import metapydonhive, swpydonhive, minihiveosc, minihivejunxion
   #from pydon import lmpydonhive
   ##from lmpydonhive import *
 #except:
-pydon_have_libmapper = False
+  #pydon_have_libmapper = False
   ##print( "libmapper not available" )
+  
+import os
+import sys
+import logging
+import logging.handlers as handlers
 
+
+class LogFile(object):
+    """File-like object to log text using the `logging` module."""
+
+    def __init__(self, options, name=None):
+        self.logger = logging.getLogger(name)
+	#formatter = logging.Formatter('%(asctime)s %(levelname)s\t%(message)s')
+	#formatter = logging.Formatter('%(asctime)s %(levelname)s\t%(message)s')
+	level = logging.__dict__.get(options.loglevel.upper(),logging.DEBUG)
+	self.logger.setLevel(level)
+	 # Output logging information to screen
+	if not options.quiet:
+	  hdlr = logging.StreamHandler(sys.stderr)
+	  #hdlr.setFormatter(formatter)
+	  self.logger.addHandler(hdlr)
+
+	# Output logging information to file
+	logfile = os.path.join(options.logdir, options.logname )
+	if options.clean and os.path.isfile(logfile):
+	    os.remove(logfile)
+	hdlr2 = handlers.RotatingFileHandler(logfile, maxBytes=5242880, backupCount=5)  #max 5 megabytes per file
+	#hdlr2 = logging.FileHandler(logfile)
+	#hdlr2.setFormatter(formatter)
+	self.logger.addHandler(hdlr2)
+
+    def write(self, msg, level=logging.INFO):
+        self.logger.log(level, msg)
+
+    def flush(self):
+        for handler in self.logger.handlers:
+            handler.flush()
+
+    
 #mpd = pydon.metapydonhive.MetaPydonHive()
 mpd = metapydonhive.MetaPydonHive()
-mpd.readOptions()
+options = mpd.readOptions()
+
+logfile = LogFile( options, 'stdoutAndErr')
+sys.stdout = logfile
+sys.stderr = logfile
+
 mpd.writeOptions()
 mpd.startHive()
