8ed8005c6daaae65bdf0d31e8b26d4cf45d74072
Marije Baalman <sensestage@nescivi.nl>
2016-04-27 15:26:50 +0200

    update windows scripts and instructions

--- windows/install_pydon_2.bat
+++ windows/install_pydon_2.bat
@@ -4,23 +4,22 @@ cd ssdn_python-master
 
 echo "installing pyserial"
 cd pyserial-sensestage
-python.exe setup.py install
+C:\Python27\python.exe setup.py install
 cd ..
 
 echo "installing xbee"
 cd XBee-2.0.1
-python.exe setup.py install
+C:\Python27\python.exe setup.py install
 cd ..
 
 echo "installing pyosc"
 cd pyosc
-python.exe setup.py install
+C:\Python27\python.exe setup.py install
 cd ..
-
 
 echo "installing pydon"
 cd pydon
-python.exe setup.py install
+C:\Python27\python.exe setup.py install
 cd ..
 
 echo "done installing"
---
+++ windows/start_pydon_2.bat
@@ -0,0 +1,5 @@
+cd %~dp0
+cd ssdn_python-master
+cd pydon
+cd scripts
+C:\Python27\python.exe pydongui.py
